cfde6025e4078219e2c64c9676b36e5e6090781e
gendzmin <gendzmin@gmail.com>
2023-11-12 00:48:24 +0300

    adding timer logic and refactoring index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,14 @@ const firebaseConfig = {
     storageBucket: "slider-game-database.appspot.com",
     messagingSenderId: "99994989759",
     appId: "1:99994989759:web:acb79d25a1d12fefea420a"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const usersDataDB = firebase.database().ref('userData');
+firebase.initializeApp(firebaseConfig);
+const usersDataDB = firebase.database().ref('usersData');
 
-
+// Логика приложения в функции app
+// Был бы рад, если бы кто-то разнёс месиво внутри по MVC
+const app = () => {
   // Стейт конкретной сессии
   const state = {
     playerName: null,
@@ -21,7 +23,7 @@ const firebaseConfig = {
     timePassed: null,
   };
 
-  // Получение списка пользователей из БД
+    // Получение списка пользователей из БД
   const getUsersList = () => {
     return new Promise((resolve, reject) => {
       const usersList = [];
@@ -36,9 +38,8 @@ const firebaseConfig = {
       });
     });
   };
-  
 
-  // Создание элемента-лидерборда
+    // Создание элемента-лидерборда
   const createLeaderboard = () => {
     const ulEl = document.createElement('ul');
     ulEl.classList.add('ul-el', 'user-list');
@@ -53,33 +54,38 @@ const firebaseConfig = {
     return ulEl;
   };
 
-  // Рендер данных
-  const render = (state) => {
-    document.querySelector('#player-name').textContent = `Ваше имя: ${state.playerName}`;
+  // Внесение имени игрока
+  state.playerName = prompt('Как вас зовут?');
+  
+  // Инициирующий рендер
+  const initRender = (state) => {
+    document.querySelector('#player-name').textContent = `Ваше имя: ${state.playerName ?? 'Аноним'}`;
     document.querySelector('#leaderboard').appendChild(createLeaderboard());
-  };
-
-  // Где-то здесь - функция логики игры, меняющая стейт в зависимости от действий игрока
-  const initGame = () => {
-    state.playerName = prompt('Как вас зовут?');
-    render(state);
-  };
-  initGame();
-
-  // Добавление данных в БД по кнопке
-document.querySelector('#save-game').addEventListener('click', (e) => {
-  alert('Данные отправлены!');
-  const newData = usersDataDB.push();
-  newData.set(state);
-});
-
-// Получение списка пользователей из БД по кнопке
-document.querySelector('#get-users').addEventListener('click', (e) => {
-  usersDataDB.once('value')
-    .then(function(snapshot) {
-      const usersData = snapshot.val();
-      for (const userID in usersData) {
-        console.log(usersData[userID].playerName);
+    document.addEventListener("DOMContentLoaded", function () {
+      let startDate = new Date();
+      const updateTimer = () => {
+        const currentDate = new Date();
+        const passedSeconds = Math.floor((currentDate - startDate) / 1000);
+        const formattedTime = `${Math.floor(passedSeconds / 60)}:${(passedSeconds % 60) < 10 ? '0' : ''}${passedSeconds % 60}`;
+        state.timePassed = formattedTime;
+        render(state);
       }
-    })
-});
+      setInterval(updateTimer, 1000);
+    });
+    document.querySelector('#start-game-button').addEventListener('click', (e) => {
+      alert('Данные отправлены!');
+      const newData = usersDataDB.push();
+      newData.set(state);
+    });
+  };
+  initRender(state);
+
+  // Рендер, регулярно обновляющий вью
+  const render = (state) => {
+    document.querySelector('.timer').textContent = state.timePassed;
+  };
+
+};
+app();
+
+export default app;
