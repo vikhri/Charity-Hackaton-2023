46cc3df6e5a073870e131689d99b5fbb7b569f79
vikhri <vikhrova.irina@gmail.com>
2023-11-11 20:37:13 +0300

    добавлен рендеринг положения карточек

--- src/js/GameEvents.js
+++ src/js/GameEvents.js
@@ -1,13 +1,44 @@
 import Board from "./Board.js";
 
-const elem = document.getElementById('app');
+const startGameButton = document.getElementById('start-game-button');
+const gameBoard = document.getElementById('game-board');
 const cells = document.querySelectorAll('.cell');
 
 const board = new Board([]);
 
-elem.addEventListener('click', () => {
-  board.randomize(4);
-  console.log(board.board)
+startGameButton.addEventListener('click', () => {
+  board.randomize(3);
+  console.log(board.board);
+
+  const flatBoard = board.board.flat();
+  const className = 'cell-3x3'
+  let content = [];
+
+
+
+
+  flatBoard.forEach((item) => {
+
+    let image = item === 0 ? '' : `<img src="src/images/photo-Field/${3}/${item}.jpg">`;
+
+    content.push(
+      `<div class="cell ${className}">` +
+      `<div class="image-wrapper">` +
+      image +
+      `</div>` +
+      `</div>`
+    )
+  })
+
+
+  gameBoard.innerHTML = content.join('');
+
+
+
+
+
+
+
 })
 
 cells.forEach((cell) => {
